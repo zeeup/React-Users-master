eb6ed7b5dd1c8ce7f8278a43e382a5d39044ce43
HAMAYOUNMASROOR <hamayounmasroor29@gmail.com>
2022-01-08 18:05:13 +0500

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+        The Pakistan Cricket Board (PCB) is the central industry 
+        for cricket in Pakistan which deals with all the concerned
+         matters of Pakistan cricket and Pakistan Cricket Services.
+        </p>
+        <p className="lead">
+        The Pakistan Cricket Board (PCB) is the central industry 
+        for cricket in Pakistan which deals with all the concerned
+         matters of Pakistan cricket and Pakistan Cricket Services.
+        </p>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default About;
